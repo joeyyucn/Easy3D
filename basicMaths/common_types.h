18ceb762054b4a9bc4dc6157bb76566fbf2b49e7
Yu <JYu2@activision.com>
2018-03-02 19:23:57 +0800

    add line intersection test function

--- basicMaths/common_types.h
+++ basicMaths/common_types.h
@@ -123,6 +123,7 @@ enum
 	PARAM_LINE_NO_INTERSECT,
 	PARAM_LINE_INTERSECT_IN_SEGMENT,
 	PARAM_LINE_INTERSECT_OUT_SEGMENT,
+	PARAM_LINE_INTERSECT_EVERYWHERE,
 };
 FORCE_INLINE int Intersect_Param_Lines2D( PARAMLINE2D_PTR p1, PARAMLINE2D_PTR p2, float* t1, float* t2 )
 {
@@ -184,4 +185,59 @@ FORCE_INLINE void Compute_Param_Line3D( PARAMLINE3D_PTR p, float t, POINT3D_PTR 
 	pt->x = p->p0.x + t * p->v.x;
 	pt->y = p->p0.y + t * p->v.y;
 	pt->z = p->p0.z + t * p->v.z;
+}
+
+FORCE_INLINE void PLANE3D_INIT( PLANE3D_PTR p, POINT3D_PTR p0, VECTOR3D_PTR normal, int normalize )
+{
+	p->p.x = p0->x;
+	p->p.y = p0->y;
+	p->p.z = p0->z;
+
+	p->n.x = normal->x;
+	p->n.y = normal->y;
+	p->n.z = normal->z;
+
+	if( normalize )
+	{
+		VECTOR3D_NORMALIZE( &p->n , &p->n );
+	}
+}
+
+// calculate if a point is in a plane, positive\negative space
+// return:
+// 0: if the point is in plane
+// >0: if the point is in the positive space
+// <0: if the point is in the negative space
+// basiclly this is a dot product operation and the value would be 
+// depend on the angle to normal vector
+FORCE_INLINE float Compute_Point_In_Plane3D( PLANE3D_PTR p, POINT3D_PTR pt )
+{
+	return p->n.x * ( pt->x - p->p.x ) + p->n.y * ( pt->y - p->p.y ) + p->n.z * ( pt->z - p->p.z );
+}
+
+FORCE_INLINE int Intersect_Param_Line3D_Plane3D( PLANE3D_PTR p, PARAMLINE3D_PTR l, float* t )
+{
+	float nv = VECTOR3D_DOT( &p->n, &l->v );
+	if( nv == 0 )
+	{
+		if( Compute_Point_In_Plane3D( p, &l->p0 ) == 0.0f )
+		{
+			return PARAM_LINE_INTERSECT_EVERYWHERE;
+		}
+		else
+		{
+			return PARAM_LINE_NO_INTERSECT;
+		}
+	}
+
+	*t = (p->p.x - l->p0.x) * p->n.x + ( p->p.y - l->p0.y ) * p->n.y + ( p->p.z - l->p0.z ) * p->n.z / nv;
+	if( *t >= 0 && *t <= 1 )
+	{
+		return PARAM_LINE_INTERSECT_IN_SEGMENT;
+	}
+	else
+	{
+		return PARAM_LINE_INTERSECT_OUT_SEGMENT;
+	}
+	
 }
